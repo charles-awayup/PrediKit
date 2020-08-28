aa6147789a36b82f200865430e7b31bbd05e9451
Bo Weber <bo.weber@tu-dortmund.de>
2020-08-28 17:00:01 +0200

    Added SPM support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "PrediKit"
+   name: "PrediKit",
+   products: [
+      .library(
+         name: "PrediKit",
+         targets: ["PrediKit"]),
+   ],
+   dependencies: [],
+   targets: [
+      .target(
+         name: "PrediKit",
+         dependencies: [],
+         path: "Sources"),
+      .testTarget(
+         name: "PrediKitTests",
+         dependencies: ["PrediKit"],
+         path: "Tests"),
+   ]
 )
